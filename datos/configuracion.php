465ef2da9b6ccbfb9cb5762024d1a331a45a4558
JOSE GUEAVARA <44011486+josemgued@users.noreply.github.com>
2018-11-08 08:05:05 -0500

    Update configuracion.php

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -1,18 +1,16 @@
 <?php 
 
 
-/*
-/**CONF. NET
+/*CONF. NET*/
 define("TIPO_BD","postgres"); //postgres,mysql
 define("SERVIDOR_BD", "ec2-54-225-68-133.compute-1.amazonaws.com");
 define("USUARIO_BD", "upcsapkmmjtcie");
 define("CLAVE_BD", "948c8eb874fcdd1e41228b652d8b76e3dd51c81b0a5bd0170f3026a3f5c56585");
 define("PUERTO_BD", "5432");
 define("NOMBRE_BD", "dbu6cbrqmluv6t");
-*/
 
 /**CONF. LOCAL*/
-
+/*
 define("TIPO_BD","postgres"); //postgres,mysql
 define("SERVIDOR_BD", "localhost");
 define("USUARIO_BD", "postgres");
@@ -20,3 +18,4 @@ define("CLAVE_BD", "postgres");
 define("PUERTO_BD", "5432");
 define("NOMBRE_BD", "bd_jose");
 
+*/
