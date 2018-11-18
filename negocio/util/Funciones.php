d83776a256cf858a024de123fec4207faffda305
JOSE GUEAVARA <44011486+josemgued@users.noreply.github.com>
2018-11-18 12:44:51 -0500

    Add files via upload

--- negocio/util/Funciones.php
+++ negocio/util/Funciones.php
@@ -387,10 +387,11 @@ class Funciones {
             
     }
 
+/*
      public static function enviarCorreo($asunto,$cuerpo,$de,$para){                 
             $headers = "MIME-Version: 1.0\r\n";
             $headers .= "Content-type: text/html; charset=utf-8\r\n";
-            $headers .= "From: ".trim($de);   /*correo de */
+            $headers .= "From: ".trim($de);   //correo de
             if (mail(trim($para),$asunto,$cuerpo,$headers)){
                 $registros["estado"] = 200;  
                 $registros["mensaje"] = 'Mensaje enviado';  
@@ -400,7 +401,73 @@ class Funciones {
                 
             }
             return $registros;
-   }
+   }*/
+
+    public static function enviarCorreo($asunto,$cuerpo,$de,$para){    
+        date_default_timezone_set('Etc/UTC');
+
+        //Create a new PHPMailer instance
+        $mail = new PHPMailer;
+
+        //Tell PHPMailer to use SMTP
+        $mail->isSMTP();
+
+        //Enable SMTP debugging
+        // 0 = off (for production use)
+        // 1 = client messages
+        // 2 = client and server messages
+        $mail->SMTPDebug = 0;
+
+        //Ask for HTML-friendly debug output
+        $mail->Debugoutput = 'html';
+
+        //Set the hostname of the mail server
+        $mail->Host = 'smtp.gmail.com';
+        // use
+        // $mail->Host = gethostbyname('smtp.gmail.com');
+        // if your network does not support SMTP over IPv6
+
+        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+        $mail->Port = 587;
+
+        //Set the encryption system to use - ssl (deprecated) or tls
+        $mail->SMTPSecure = 'tls';
+
+        //Whether to use SMTP authentication
+        $mail->SMTPAuth = true;
+
+        //Username to use for SMTP authentication - use full email address for gmail
+        $mail->Username = "erik.ur.gf.10@gmail.com";
+
+        //Password to use for SMTP authentication
+        $mail->Password = "DESCANSOODIAME";
+
+        //Set who the message is to be sent from
+        $mail->setFrom($de, 'APP');
+
+        //Set who the message is to be sent to
+        $mail->addAddress(trim($para), 'Usuario');
+
+        //Set the subject line
+        $mail->Subject = $asunto;
+
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        //$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+        $mail->msgHTML($cuerpo);
+
+        if (!$mail->send()) {
+            $registros["estado"] = 500;  
+            $registros["mensaje"] = "Problema al enviar correo ".$mail->ErrorInfo;  
+        } else {
+            $registros["estado"] = 200;  
+            $registros["mensaje"] = 'Mensaje enviado';
+        }
+
+        return $registros;
+    }
+
+
 
    public static function mimeToExt($mime) {
     $extensions = array(
