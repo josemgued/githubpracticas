ed037dcd74b805e0d30b4a78cb80d624a57dd681
José Miguel <44011486+josemgued@users.noreply.github.com>
2021-11-09 03:29:54 -0500

    Update Funciones.php

--- negocio/util/Funciones.php
+++ negocio/util/Funciones.php
@@ -406,13 +406,7 @@ class Funciones {
     public static function enviarCorreo($asunto,$cuerpo,$de,$para){    
         date_default_timezone_set('Etc/UTC');
         
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
         
-        require '../../PHPMailer-master/src/Exception.php';
-        require '../../PHPMailer-master/src/PHPMailer.php';
-        require '../../PHPMailer-master/src/SMTP.php';
     
         //use PHPMailer\PHPMailer\PHPMailer;
         //use PHPMailer\PHPMailer\Exception;
